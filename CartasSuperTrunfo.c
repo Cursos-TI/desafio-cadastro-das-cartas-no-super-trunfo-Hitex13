cb3a353d882aefdc262dbf5f35074d8eefa91dad
Vinicius de Andrade Sampaio <cobracaptali@gmail.com>
2025-03-24 19:33:39 +0000

    Minha atividade sobre o jogo das CartasSuperTrunfo utilizei o fgets inves do scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,71 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char Estado[20];
+char cidade[20]; 
+int codigo;
+int habitantes;
+float area;
+float pib;
+int turisticos;
+
+char Estado2[20];
+char cidade2[20]; 
+int codigo2;
+int habitantes2;
+float area2;
+float pib2;
+int turisticos2;
+printf("Bem vindo ao jogo das Cartas\n");
+
+printf ("digite  um estado. \n");
+fgets(Estado,sizeof(Estado),stdin);
+
+printf ("Digite uma Cidade. \n");
+fgets (cidade,sizeof(cidade), stdin);
+
+printf("Digite uma numeração. \n");
+scanf("%d", &codigo);
+
+printf("Digite o numero de Habitantes dessa cidade. \n");
+scanf("%d", &habitantes );
+
+printf("Digite Area dessa cidade Em Km \n");
+scanf("%f", &area);
+
+printf("Digite o Pib dessa cidade.\n");
+scanf("%f", &pib);
+
+printf("Digite a quantidade de pontos turisticos.\n");
+scanf("%d", &turisticos);
+
+printf("|Codigo da Carta - %.1s0%d| \n|Estado - %s|\n|Cidade - %s|\n|Numeros de Habitante -%d|\n|Area dessa cidade - %.2f Km|\n|Pib - %.2f Bilhoes|\n|Quantidade de pontos turisticos - %d|", Estado , codigo , Estado , cidade, habitantes, area, pib, turisticos );
+
+printf ("Digite outro  estado. \n");
+fgets(Estado2,sizeof(Estado2),stdin);
+
+printf ("Digite outra Cidade. \n");
+fgets (cidade2,sizeof(cidade2), stdin);
+
+printf("Digite outra numeração. \n");
+scanf("%d", &codigo2);
+
+printf("Digite o numero de Habitantes dessa cidade. \n");
+scanf("%d", &habitantes2 );
+
+printf("Digite Area dessa cidade Em Km \n");
+scanf("%f", &area2);
+
+printf("Digite o Pib dessa cidade.\n");
+scanf("%f", &pib2);
+
+printf("Digite a quantidade de pontos turisticos.\n");
+scanf("%d", &turisticos2);
+
+printf("Segunda Carta \n|Codigo da Carta - %.1s0%d| \n|Estado - %s|\n|Cidade - %s|\n|Numeros de Habitante -%d|\n|Area dessa cidade - %.2f Km|\n|Pib - %.2f Bilhoes|\n|Quantidade de pontos turisticos - %d|", Estado2 , codigo2 , Estado2 , cidade2, habitantes2, area2, pib2, turisticos2 );
+
+printf("Muito obrigado");
+
 
     return 0;
 }
