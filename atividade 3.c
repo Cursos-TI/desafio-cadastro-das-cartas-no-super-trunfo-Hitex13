de12b70e605eaf07ac5d216e6f856c9ab58b8421
Vinicius de Andrade Sampaio <cobracaptali@gmail.com>
2025-04-03 16:42:58 -0300

    CartasSupertrunfo  3.c
    Estava dando error na Carta 2 depois de um tempo

--- atividade 3.c
+++ atividade 3.c
@@ -2,65 +2,70 @@
 
 int main (){
 
-char Estado1[20] ,cidade1[20] ,Estado2[20] ,cidade2[20];
-int Populaçao1, Codigo1 ,Turisticos1 ,Populaçao2 ,Codigo2 ,Turisticos2;
-float Pib1 ,area1 ,Pib2 ,area2 ,Densidade ,Capita ,Densidade2 ,Capita2;
+char Estado1[40] ,cidade1[40] ;
+int Populaçao1, Codigo1 ,Turisticos1 ;
+float Pib1 ,area1 ,Densidade ,Capita ;
 printf ("Carta 1\n");
 
 printf("Digite o Estado De Sua carta\n");
-fgets(Estado1,sizeof(Estado1),stdin);
+fgets(Estado1, sizeof(Estado1), stdin);
 
 printf("Digite agr a cidade da sua carta\n");
-fgets(cidade1,sizeof(cidade1),stdin);
+fgets(cidade1, sizeof(cidade1), stdin);
 
 printf("Digite o Codigo de sua carta\n");
-scanf("%d", &Codigo1);
+scanf(" %d", &Codigo1);
 
 printf("Digite a Populaçao dessa cidade\n");
-scanf("%d", &Populaçao1);
+scanf(" %d", &Populaçao1);
 
 printf("Digite a Area\n");
-scanf("%f", &area1);
+scanf(" %f", &area1);
 
 printf("Digite o Pib \n");
-scanf("%f", &Pib1);
+scanf(" %f", &Pib1);
 printf("Quantos pontos turisticos essa cidade tem\n");
-scanf("%d", &Turisticos1);
+scanf(" %d", &Turisticos1);
 
+Densidade = (float)Populaçao1/area1;
+Capita = (float)Pib1/Populaçao1;
 
+printf("|Codigo - %.1s0%d\n|Estado - %s|Cidade - %s|Habitantes - %d\n|Area - %.2f\n",Estado1 ,Codigo1 ,Estado1 ,cidade1 ,Populaçao1 ,area1 );
+printf("|Pontos Turisticos - %d\n|Densidade Populacional - %.4f Hab.Km2 \n|Pib per Capita - %f Reais\n \n",Turisticos1 ,Densidade, Capita);  
 
+getchar();
 
-
-Densidade = Populaçao1/area1;
-Capita = Pib1/Populaçao1;
-printf("|Codigo - %.1s0%d\n|Estado - %s|Cidade - %s|Habitantes - %d\n|Area - %.2f\n|Pontos Turisticos - %d\n|Densidade Populacional - %.4f Hab.Km2\n|Pib per Capita - %.2f Reais",Estado1 ,Codigo1 ,Estado1 ,cidade1 ,Populaçao1 ,area1 ,Turisticos1 ,Densidade ,Capita);
-
+char Estado2[40] ,cidade2[40];
+int Populaçao2 ,Codigo2 ,Turisticos2;
+float area2 ,Pib2, Densidade2 ,Capita2;
 
 printf ("Carta 2\n");
 
 printf("Digite o Estado De sua segunda carta\n");
-fgets(Estado2,sizeof(Estado2),stdin);
+fgets(Estado2, sizeof(Estado2), stdin);
 
 printf("Digite agr a cidade da sua carta\n");
-fgets(cidade2,sizeof(cidade2),stdin);
+fgets(cidade2, sizeof(cidade2), stdin);
+
+
 
 printf("Digite o Codigo de sua carta\n");
-scanf("%d", &Codigo2);
+scanf(" %d", &Codigo2);
 
 printf("Digite a Populaçao dessa cidade\n");
-scanf("%d", &Populaçao2);
+scanf(" %d", &Populaçao2);
 
 printf("Digite a Area\n");
-scanf("%f", &area2);
+scanf(" %f", &area2);
 
 printf("Digite o Pib \n");
-scanf("%f", &Pib2);
+scanf(" %f", &Pib2);
 printf("Quantos pontos turisticos essa cidade tem\n");
-scanf("%d", &Turisticos2);
+scanf(" %d", &Turisticos2);
 
-Densidade2 = Populaçao2/area2;
-Capita2 = Pib2/Populaçao2;
-printf("|Codigo - %.1s0%d\n|Estado - %s|Cidade - %s|Habitantes - %d\n|Area - %.2f\n|Pontos Turisticos - %d\n|Densidade Populacional - %.4f Hab.Km2\n|Pib per Capita - %.2f Reais",Estado2 ,Codigo2 ,Estado2 ,cidade2 ,Populaçao2 ,area2 ,Turisticos2 ,Densidade2 ,Capita2);
+Densidade2 = (float)Populaçao2/area2;
+Capita2 = (float)Pib2/Populaçao2;
+printf("|Codigo - %.1s0%d\n|Estado - %s|Cidade - %s|Habitantes - %d\n|Area - %.2f\n|Pontos Turisticos - %d\n|Densidade Populacional - %.4f Hab.Km2\n|Pib per Capita - %.2f Reais\n\n",Estado2 ,Codigo2 ,Estado2 ,cidade2 ,Populaçao2 ,area2 ,Turisticos2 ,Densidade2 ,Capita2);
 
 printf("\nMuito Obrigado Por utilizar o Meu codigo ");
 
